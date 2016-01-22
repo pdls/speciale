eb4058dd9bf49626a951cffe11a0519a9c94beb9
pdls <peterdamlundgaardsoerensen@gmail.com>
2016-01-22 15:49:00 +0100

    tilføjet mat.py

---
+++ mat.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+#import math
+import numpy as np
+
+plt.close('all') # close all figures
+
+x = np.linspace(np.pi/2, np.pi, 1001)
+x1 = [0.5, 0.5]
+x2 = [0.6, 0.6]
+y1 = [0 , 0.5]
+y2 = [0 , 0.6]
+plt.plot(np.sin(x), - np.cos(x))
+plt.plot(np.sin(x1), -np.cos(y1))
+plt.plot(np.sin(x2), -np.cos(y2))
+plt.show()
